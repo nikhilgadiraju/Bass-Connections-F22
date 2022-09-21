1eb2463a79222dd22719740d6f5ddb45d1d87f31
Nikhil <nikhil.gadiraju@duke.edu>
2022-09-21 12:02:16 -0400

    Create temporary python file to read the posthoc_standard CSV and sort FRD-corrected P values to identify the relevant brain regions

---
+++ Statistical Analysis/read_posthoc.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv("/Volumes/GoogleDrive/My Drive/Education School/Duke University/Year 4 (2022-2023)/Courses/Semester 1/BME 493 (Badea Independent Study)/Bass-Connections-F22/Statistical Analysis/posthoc_standard.csv")
+df_FDR = df.sort_values(by=['FDR'], ascending=True)
+print(df)
