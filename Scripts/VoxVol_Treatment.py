eebee69197598640d9cd7d8b608bb160dc3792ff
Nikhil <nikhil.gadiraju@duke.edu>
2022-09-21 14:33:15 -0400

    Create .py file to add treatment column and add initial import code (import pandas, voxvol dataset, and treatment CSV)

---
+++ Scripts/VoxVol_Treatment.py
@@ -0,0 +1,10 @@
+# VoxVol_Treatment.py
+# Author: Nikhil Gadiraju
+
+# %% Imports
+import pandas as pd
+
+# %% Data Imports
+vox_vol_data = pd.read_csv("/Users/nikhilgadiraju/Box Sync/Home Folder nvg6/Sharing/Bass Connections/Processed Data/Mean Intensity & Voxel Volumes/voxelvolumes.csv")
+treatment_data = pd.read_csv("/Volumes/GoogleDrive/My Drive/Education School/Duke University/Year 4 (2022-2023)/Courses/Semester 1/BME 493 (Badea Independent Study)/Bass-Connections-F22/Reference Files/QCLAB_AD_mice011222.csv")
+
