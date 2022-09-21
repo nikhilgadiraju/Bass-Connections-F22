121222763c13e8f6f49fa9efb6ce4903c5fbda6f
Nikhil <nikhil.gadiraju@duke.edu>
2022-09-21 14:54:18 -0400

    Isolate columns of interest and filter through rows that have treatment conditions of 'treadmill', 'wheel_only', and 'sedentary'

--- Scripts/VoxVol_Treatment.py
+++ Scripts/VoxVol_Treatment.py
@@ -1,10 +1,16 @@
 # VoxVol_Treatment.py
 # Author: Nikhil Gadiraju
 
-# %% Imports
+# %% PACKAGE/MODULE IMPORTS
 import pandas as pd
 
-# %% Data Imports
+# %% DATA IMPORTS
+# FILE: voxelvolumes.csv
 vox_vol_data = pd.read_csv("/Users/nikhilgadiraju/Box Sync/Home Folder nvg6/Sharing/Bass Connections/Processed Data/Mean Intensity & Voxel Volumes/voxelvolumes.csv")
+# FILE: QCLAB_AD_mice011222.csv
 treatment_data = pd.read_csv("/Volumes/GoogleDrive/My Drive/Education School/Duke University/Year 4 (2022-2023)/Courses/Semester 1/BME 493 (Badea Independent Study)/Bass-Connections-F22/Reference Files/QCLAB_AD_mice011222.csv")
+
+# %% DATA PROCESSING
+id_treatment_df = treatment_data[["Animal", "Treatment"]] # Isolate Animal ID and Treatment columns
+filtered_df = id_treatment_df[id_treatment_df["Treatment"].isin(['sedentary','wheel_only','treadmill'])]
 
