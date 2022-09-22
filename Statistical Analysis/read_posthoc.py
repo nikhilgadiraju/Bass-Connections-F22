cd5f225bd55b485cae6f71a2611bfd0cfa587f80
Nikhil <nikhil.gadiraju@duke.edu>
2022-09-22 17:37:57 -0400

    Reorganized code to succesefully read R-file output CSV and sort based on comparison group P-value

--- Statistical Analysis/read_posthoc.py
+++ Statistical Analysis/read_posthoc.py
@@ -1,4 +1,10 @@
+# VoxVol_Treatment.py
+# Author: Nikhil Gadiraju
+
+# %% PACKAGE/MODULE IMPORTS
 import pandas as pd
+
+# %% DATA IMPORTS
 df = pd.read_csv("/Volumes/GoogleDrive/My Drive/Education School/Duke University/Year 4 (2022-2023)/Courses/Semester 1/BME 493 (Badea Independent Study)/Bass-Connections-F22/Statistical Analysis/posthoc_standard.csv")
 df_FDR = df.sort_values(by=['FDR'], ascending=True)
-print(df)
+print(df_FDR)
