a891a43234bc9c46feaf411e8d402f961e6741f4
Nikhil <nikhil.gadiraju@duke.edu>
2022-09-27 11:54:49 -0400

    Printed first violin plot in R via ggplot2 package

--- Statistical Analysis/humanreadandrun.R
+++ Statistical Analysis/humanreadandrun.R
@@ -3,6 +3,7 @@ library(data.table)
 library(readxl)
 library(rstatix)
 library(jmv)
+library(ggplot2)
 
 # Read voxel volumes w/ treatments excel file; remove the first 3 columns (index, filename, ID) and leave treatment and data columns; remove NaNs
 data=read.csv('/Users/nikhilgadiraju/Box Sync/Home Folder nvg6/Sharing/Bass Connections/Processed Data/Mean Intensity & Voxel Volumes/voxelvolumes.csv')
@@ -54,3 +55,13 @@ for (i in 1:length(sig)) {
 colnames(posthoc)=c("FDR","ST","SW","TW")
 rownames(posthoc)=names(sig)
 write.csv(posthoc, 'posthoc_standard.csv')
+
+# Read top regions CSVs
+top_st=read.csv('/Volumes/GoogleDrive/My Drive/Education School/Duke University/Year 4 (2022-2023)/Courses/Semester 1/BME 493 (Badea Independent Study)/Bass-Connections-F22/Reference Files/User-generated Files/top_regions_st.csv')
+
+# Basic Violin Plots
+sig_reg = top_st$Abbreviation[1]
+p <- ggplot(data, aes_string(x="Treatment", y=sig_reg)) + 
+  geom_violin()
+p
+
