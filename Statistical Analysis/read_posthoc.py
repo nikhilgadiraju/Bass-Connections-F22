ca10eca91b66f64cbee546b4a1ca40f86728ce71
Nikhil <nikhil.gadiraju@duke.edu>
2022-10-04 18:01:36 -0400

    Update top_regs CSVs as well as read_posthoc.py script to output top 10 regions sorted by positive and negative effect sizes

--- Statistical Analysis/read_posthoc.py
+++ Statistical Analysis/read_posthoc.py
@@ -42,20 +42,30 @@ df_vol = pd.read_csv("/Users/nikhilgadiraju/Box Sync/Home Folder nvg6/Sharing/Ba
 
 # %% DATA PROCESSING
 # Utilize the defined functions to create updated structures and abbreviation lists to eventually store through a dictionary
-df = df.rename(columns={"ST Comparison Group Pvalue": 'ST', 'SW Comparison Group Pvalue': 'SW', 'TW Comparison Group Pvalue': 'TW'})
+df = df.rename(columns={"ST Comparison Group Effect Size": 'ST', 'SW Comparison Group Effect Size': 'SW', 'TW Comparison Group Effect Size': 'TW'})
+df = df.rename(columns={"ST Comparison Group Pvalue": 'ST_p', 'SW Comparison Group Pvalue': 'SW_p', 'TW Comparison Group Pvalue': 'TW_p'})
 index_df = index.iloc[:,0:3]
 structure_updated = list(map(lambda x,y: structure_update(x,y), index_df["Structure"], index_df["Hemisphere"]))
 abbreviation_updated = list(map(lambda x,y: abbreviation_update(x,y), index_df["Abbreviation"], index_df["Hemisphere"]))
 name_dict = {abbreviation_updated[i]: structure_updated[i] for i in range(len(abbreviation_updated))}
 
 # Sort through posthoc_standard.csv to identify relevant regions for each comparison group based on corrected p-value
-top_regs = []
+top_regs_pos = []
+top_regs_neg = []
+
 top_regs_num = 10
 comp_groups = ['ST','SW','TW']
 for comparison in comp_groups:
-    df_comp = df.sort_values(by=[comparison], ascending=True)
-    top = pd.DataFrame({'Abbreviation': df_comp.iloc[:,0], 'Structure': [name_dict[key] for key in df_comp.iloc[:,0]], 'p-values': df_comp.loc[:,comparison]}).head(top_regs_num)
-    top_regs.append(top) # Append relevant regions based on comparison group to empty top_regs list
+    df_iso = df.loc[:,["Unnamed: 0","{}_p".format(comparison),comparison]]
+    df_sig = df_iso[df_iso["{}_p".format(comparison)] <= 0.05]
+
+    df_pos = df_sig.sort_values(by=[comparison], ascending=False)
+    top_pos = pd.DataFrame({'Abbreviation': df_pos.iloc[:,0], 'Structure': [name_dict[key] for key in df_pos.iloc[:,0]], 'P-value': df_pos.loc[:,"{}_p".format(comparison)], 'Effect Size': df_pos.loc[:,comparison]}).head(top_regs_num)
+    top_regs_pos.append(top_pos) # Append relevant regions based on comparison group to empty top_regs list
+
+    df_neg = df_sig.sort_values(by=[comparison], ascending=True)
+    top_neg = pd.DataFrame({'Abbreviation': df_neg.iloc[:, 0], 'Structure': [name_dict[key] for key in df_neg.iloc[:, 0]], 'P-value': df_neg.loc[:, "{}_p".format(comparison)], 'Effect Size': df_neg.loc[:, comparison]}).head(top_regs_num)
+    top_regs_neg.append(top_neg)
 
 # Replace naming convention for different treatment groups to more accurately describe exercise conditions
 df_vol = df_vol.replace({"Treatment": "wheel_only"}, "voluntary")
@@ -64,7 +74,9 @@ df_vol = df_vol.replace({"Treatment": "treadmill"}, "voluntary + enforced")
 # %% DATAFRAME OUTPUT
 os.chdir("/Volumes/GoogleDrive/My Drive/Education School/Duke University/Year 4 (2022-2023)/Courses/Semester 1/BME 493 (Badea Independent Study)/Bass-Connections-F22/Reference Files/User-generated Files")
 for ind in range(len(comp_groups)):
-    top_regs[ind].to_csv('top_regions_{}.csv'.format(comp_groups[ind].lower()), encoding='utf-8')
+    top_regs_pos[ind].to_csv('top_regions_{}.csv'.format(comp_groups[ind].lower()), encoding='utf-8', mode='w', index=False)
+    top_regs_neg[ind].to_csv('top_regions_{}.csv'.format(comp_groups[ind].lower()), encoding='utf-8', mode='a', index=False)
+
 
 
 
